d5f0dfc2cf161292e53c7358ce5642dd927e3188
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-06 16:16:16 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,22 +1,37 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year 
+  FROM books WHERE id < 4
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto
+  FROM characters 
+  ORDER BY motto DESC LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species)
+  FROM characters
+  ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+  FROM authors
+  LEFT JOIN subgenres
+  ON authors.id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series 
+  INNER JOIN characters 
+  ON series.id = characters.series_id 
+  GROUP BY series.title
+  HAVING characters.species = 'human'
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
