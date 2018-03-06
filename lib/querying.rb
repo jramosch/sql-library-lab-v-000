cba489b253e11a86ad305613a24035bdaeaf9767
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-06 16:51:25 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,5 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(character_books.book_id)
   LEFT JOIN character_books
   ON characters.id = character_books.character_id
-  GROUP BY "
+  GROUP BY characters.name ORDER BY COUNT(character_books.book_id) DESC;"
 end
