143238e7e1efca0fdc7aebf50b18dbbd0939e3be
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-03-06 16:50:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,8 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books.character_id)
+  "SELECT characters.name, COUNT(character_books.book_id)
   LEFT JOIN character_books
-  ON"
+  ON characters.id = character_books.character_id
+  GROUP BY "
 end
